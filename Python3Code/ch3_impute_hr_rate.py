01ec50e94e49827d4d52dd6f1a3ba16a7bd4ed24
ALuesink <anneluesink@live.nl>
2020-06-06 17:35:23 +0200

    Imputing heart rate + accompaning plots

--- Python3Code/ch3_impute_hr_rate.py
+++ Python3Code/ch3_impute_hr_rate.py
@@ -1,7 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+import pandas as pd
 from Chapter3.KalmanFilters import KalmanFilters
 from util.VisualizeDataset import VisualizeDataset
-import pandas as pd
 from pathlib import Path
+
+
 
 def main():
     DATA_PATH = Path('./intermediate_datafiles/')
@@ -24,11 +28,23 @@ def main():
 
     dataset = Kalman.apply_kalman_filter(dataset, 'hr_watch_rate')
     # print(dataset.head())
-    # print(dataset.columns)
+    # print(dataset.index)
 
-    DataViz.plot_dataset(dataset, ['hr_watch_rate', 'hr_watch_rate_kalman'], ['exact','exact'], ['line', 'line'])
+    DataViz.plot_dataset(dataset, ['hr_watch_rate', 'hr_watch_rate_kalman'], ['exact', 'exact'], ['line', 'line'])
+    # DataViz.plot_xy([dataset.index], [dataset['hr_watch_rate']])
 
-    dataset.to_csv(DATA_PATH / RESULT_FNAME)
+    # xfmt = md.DateFormatter('%H:%M')
+    #
+    # fig, axs = plt.subplots(2, sharex=True, sharey=False)
+    # axs[0].xaxis.set_major_formatter(xfmt)
+    # axs[0].plot(dataset.index, 'hr_watch_rate', 'bo', data=dataset)
+    # axs[0].set_title('hr_watch_rate')
+    # axs[1].xaxis.set_major_formatter(xfmt)
+    # axs[1].plot(dataset.index, 'hr_watch_rate_kalman', 'bo', data=dataset)
+    # axs[1].set_title('hr_watch_rate_kalman')
+    # plt.show()
+
+    # dataset.to_csv(DATA_PATH / RESULT_FNAME)
 
 if __name__ == '__main__':
     main()
